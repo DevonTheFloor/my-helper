f629a790598e6111884a038f17fb6ead31b25b04
DevonTheFloor <devonthefloor@free.fr>
2022-03-15 14:51:58 +0100

    Up doc

--- myFetchHelper.js
+++ myFetchHelper.js
@@ -1,8 +1,8 @@
 /**
  * All CRUD query with asynchronous fetch, you acn have on json,xml or text. Replace format by response.json() or 'response.xml()' or 'response.text()
- * @param {*} url 
- * @param {*} init 
- * @returns JSON
+ * @param {*} url String to url data
+ * @param {*} init Object for config post request
+ * @returns JSON, XML or Text
  */
 export async function myPost(url, init, format) {
 
