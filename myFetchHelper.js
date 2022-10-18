b27b1da3fb30a88e1185c3b88cb6ed057dd333ae
DevonTheFloor <devonthefloor@free.fr>
2022-10-18 11:07:36 +0200

    Update fetchHelpers

--- myFetchHelper.js
+++ myFetchHelper.js
@@ -4,20 +4,33 @@
  * @param {*} init Object for config post request
  * @returns JSON, XML or Text
  */
-export async function myPost(url, init, format) {
-
-  let response = await fetch(url, init, format);
-  if (!response.ok) {
-    throw new Error(`Erreur transmission requête. Statut : ${response.status}`)
+ export async function myPost(url, init) {
+  let response = await fetch(url, init);
+  if (response.ok === false) {
+    throw new Error(`Erreur transmission requête. Statut : ${response.status}`);
+  } else  {
+    const resp = await response.json();
+    return Promise.resolve(resp);
   }
-  return await format;
+
 };
 
-export async function myGet(url, format) {
+export async function myGet(url) {
   let response = await fetch(url);
   if (!response.ok) {
-    throw new Error(`Erreur transmission requête. Statut : ${response.status}`)
+    throw new Error(`Erreur transmission requête. Statut : ${response.status}`);
+  } else {
+    const resp = await response.json();
+    return Promise.resolve(resp);
   }
-  return await format;
 };
 
+export async function myDelete(url) {
+  let response = await fetch(url,{methode: "DELETE"});
+  if (!response.ok) {
+    throw new Error(`Erreur transmission requête. Statut : ${response.status}`);
+  } else {
+    const resp = await response.json();
+    return Promise.resolve(resp);
+  }
+};
